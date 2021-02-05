3794e89fe14e04daa22a3052403bc3390e984833
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-02-05 13:40:00 +0800

    Change pod thrust power according to the pod direction
    (referenced to the straight line between pod and next checkpoint)

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import math
 # Auto-generated code below aims at helping you parse
 # the standard input according to the problem statement.
 
+prev_x = None
+prev_y = None
+velocity = None
+thrust = 100
 
 # game loop
 while True:
@@ -14,11 +18,39 @@ while True:
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
 
-    # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
-    # print(f"Debug {next_checkpoint_x}", file=sys.stderr, flush=True)
+
+    if velocity == None:
+        velocity = 0
+    else:
+        velocity = math.sqrt((x - prev_x)**2 + (y - prev_y)**2)
+    prev_x = x
+    prev_y = y
+
+
+    if velocity > 250 and next_checkpoint_dist < 1000:
+        thrust = 0
+    elif abs(next_checkpoint_angle) >= 60:
+        thrust = 30
+    elif abs(next_checkpoint_angle) >= 45:
+        thrust = 60
+    elif abs(next_checkpoint_angle) >= 20:
+        thrust = 80
+    else:
+        thrust = 100
 
     # You have to output the target position
     # followed by the power (0 <= thrust <= 100)
     # i.e.: "x y thrust"
-    print(f"{str(next_checkpoint_x)}  {str(next_checkpoint_y)} 100")
+    print(f"{str(next_checkpoint_x)} {str(next_checkpoint_y)} {thrust}")
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    print(f"Debug: velocity {velocity}", file=sys.stderr, flush=True)
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    print(f"Debug: target {next_checkpoint_x} {next_checkpoint_y}", file=sys.stderr, flush=True)
+
+    print(f"Debug: current {x} {y}", file=sys.stderr, flush=True)
+
+    print(f"Debug: prev {prev_x} {prev_y}", file=sys.stderr, flush=True)
