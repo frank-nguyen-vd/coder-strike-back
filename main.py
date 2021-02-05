600f2fcccd9a78d78fddd68604bda077dd61a959
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-02-05 13:17:27 +0800

    Define basic behaviour just to complete the race

---
+++ main.py
@@ -0,0 +1,24 @@
+import sys
+import math
+
+# Auto-generated code below aims at helping you parse
+# the standard input according to the problem statement.
+
+
+# game loop
+while True:
+    # next_checkpoint_x: x position of the next check point
+    # next_checkpoint_y: y position of the next check point
+    # next_checkpoint_dist: distance to the next checkpoint
+    # next_checkpoint_angle: angle between your pod orientation and the direction of the next checkpoint
+    x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
+    opponent_x, opponent_y = [int(i) for i in input().split()]
+
+    # Write an action using print
+    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
+    # print(f"Debug {next_checkpoint_x}", file=sys.stderr, flush=True)
+
+    # You have to output the target position
+    # followed by the power (0 <= thrust <= 100)
+    # i.e.: "x y thrust"
+    print(f"{str(next_checkpoint_x)}  {str(next_checkpoint_y)} 100")
