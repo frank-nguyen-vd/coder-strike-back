b4da39e5dfde3199c6683b0424d4aa4d48d8b69f
Frank Nguyen <frank.nguyen.vd@gmail.com>
2021-02-06 09:59:04 +0800

    Fix the calculation of distance to checkpoint to match in-game data

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ class Tools:
 
 class GameEnv:
     Max_Engine_Power = 100
+    Map_Width = 16000
+    Map_Height = 9000
 
     @staticmethod
     def calc_acceleration(engine_power, speed):
@@ -48,7 +50,7 @@ class Pod:
         self.orientation = orientation
         self.dist_to_chkpt = dist_to_chkpt
 
-    def update(self, x, y, orientation=None, dist_to_chkpt=None):
+    def update(self, x, y, chkpt_x, chkpt_y, orientation=None):
         self.prev_x = self.x
         self.prev_y = self.y
         self.x = x        
@@ -67,38 +69,10 @@ class Pod:
 
         self.orientation = orientation
 
-        self.dist_to_chkpt = dist_to_chkpt
+        self.dist_to_chkpt = Tools.calc_dist(x1=x, y1=y, x2=chkpt_x, y2=chkpt_y)
 
-    
-class Probe:
-    def __init__(self):
-        self.player = Pod()
-        self.opponent = Pod()
-        self.max_speed = 0
-        self.max_acceleration = 0
-
-    def update_game_info(self, player_x, player_y, opponent_x, opponent_y, chkpt_x, chkpt_y, player_orientation):
-        self.player.update(
-            x=player_x,
-            y=player_y,
-            orientation=player_orientation,
-            dist_to_chkpt=Tools.calc_dist(x1=player_x, y1=player_y, x2=chkpt_x, y2=chkpt_y))
-
-        self.opponent.update(
-            x=opponent_x,
-            y=opponent_y,            
-            dist_to_chkpt=Tools.calc_dist(x1=opponent_x, y1=opponent_y, x2=chkpt_x, y2=chkpt_y))
-
-
-    def probe_max_speed(self):
-        if self.player.speed > self.max_speed:
-            self.max_speed = self.player.speed
-
-    def probe_max_acceleration(self):
-        if abs(self.player.acceleration) > self.max_acceleration:
-            self.max_acceleration = abs(self.player.acceleration)
-
-probe = Probe()
+player = Pod()
+opponent = Pod()    
 
 # game loop
 while True:
@@ -109,16 +83,13 @@ while True:
     x, y, next_checkpoint_x, next_checkpoint_y, next_checkpoint_dist, next_checkpoint_angle = [int(i) for i in input().split()]
     opponent_x, opponent_y = [int(i) for i in input().split()]
 
-    probe.update_game_info(
-        player_x=x, player_y=x,
-        opponent_x=opponent_x, opponent_y=opponent_y,
-        chkpt_x=next_checkpoint_x, chkpt_y=next_checkpoint_y,
-        player_orientation=next_checkpoint_angle)
-
-    engine_power = 50
-    debug(f"engine_power {engine_power} speed {probe.player.speed:.2f} acc {probe.player.acceleration:.2f}")
+    player.update(x=x, y=y, chkpt_x=next_checkpoint_x, chkpt_y=next_checkpoint_y, orientation=next_checkpoint_angle)
+    opponent.update(x=opponent_x, y=opponent_y, chkpt_x=next_checkpoint_x, chkpt_y=next_checkpoint_y)
+    
+    debug(f"game dist {next_checkpoint_dist} calc {player.dist_to_chkpt}")
 
     # You have to output the target position
     # followed by the engine_power (0 <= engine_power <= 100)
     # i.e.: "x y engine_power"
+    engine_power = 50
     print(f"{str(next_checkpoint_x)} {str(next_checkpoint_y)} {engine_power}")
